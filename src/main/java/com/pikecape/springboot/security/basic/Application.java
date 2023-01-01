f11ca94989c47e2dce906205ef9ef1eed1454939
Mika Korpela <mika.korpela@gofore.com>
2023-01-01 14:06:06 +0200

    Initial commit

---
+++ src/main/java/com/pikecape/springboot/security/basic/Application.java
@@ -0,0 +1,13 @@
+package com.pikecape.springboot.security.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication
+@EnableWebSecurity
+public class Application {
+  public static void main(String... args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
